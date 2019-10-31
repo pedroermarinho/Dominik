6967d4ee2637bd7e16e5efdb28e7b7c115c058e4
Pedro Marinho <pedro.marinho238@gmail.com>
2019-10-30 22:51:47 -0400

    LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -9,8 +11,11 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from app.models import tables, forms
 from app.controllers import default
