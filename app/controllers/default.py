97a4dbbf2d2e0c5868838ed65ee1ff6268b67b50
Pedro Marinho <pedro.marinho238@gmail.com>
2019-10-30 22:52:59 -0400

    ...

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,55 +1,144 @@
-from flask import render_template
-from app import app
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_user, logout_user, login_required, current_user
+
+from app import app, db, login_manager
+
+from app.models.forms import LoginForm , RegisterForm
 
 
+from app.models.tables import User
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=user_id).first()
+
+
+@app.route('/home')
+@app.route('/index')
 @app.route('/')
 def home():
-    return render_template('index.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    else:
+        return render_template('index.html')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
-
-
-@app.route('/index')
-def index():
-    return render_template('index.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    else:
+        return render_template('about.html')
 
 
 @app.route('/blank')
 def blank():
-    return render_template('blank.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    else:
+        return render_template('blank.html')
 
 
 @app.route('/charts')
 def charts():
-    return render_template('charts.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    else:
+        return render_template('charts.html')
 
 
 @app.route('/404')
 def er404():
-    return render_template('404.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    else:
+        return render_template('404.html')
 
 
 @app.route('/forgot-password')
 def forgotpassword():
-    return render_template('forgot-password.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    else:
+        return render_template('forgot-password.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=["POST", "GET"])
 def login():
-    return render_template('login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(username=login_form.username.data).first()
+        if user and user.password == login_form.password.data:
+            login_user(user)
+            flash("Logger in")
+            return redirect(url_for("home"))
+        else:
+            flash("Invalid login")
+
+    return render_template('login.html', login_form=login_form)
 
 
-@app.route('/register')
+@app.route('/register',methods=["POST", "GET"])
 def register():
-    return render_template('register.html')
+    register_form = RegisterForm()
+    if register_form.validate_on_submit():
+        user = User.query.filter_by(username=register_form.username.data).first()
+        email = User.query.filter_by(email=register_form.email.data).first()
+        if user:
+            flash("Usuário inválido")
+        elif email:
+            flash("Email inválido")
+        else:
+            i = User(str(register_form.username.data), str(register_form.password.data), str(register_form.name.data), str(register_form.email.data))
+            db.session.add(i)
+            db.session.commit()
+            return redirect(url_for('login'))
+    
+    return render_template('register.html', register_form=register_form)
 
 
 @app.route('/tables')
 def tables():
-    return render_template('tables.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    else:
+        return render_template('tables.html')
+
+
+@app.route('/base')
+def base():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    else:
+        return render_template('base.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("Logger out.")
+    return redirect(url_for('login'))
+
+
+@app.route("/settings")
+@login_required
+def settings():
+    pass
+
+
+@app.route('/test/<info>')
+@app.route('/test/', defaults={'info': None})
+def test(info):
+    i = User("pedro123", "1234", "pedro", "pedro.marinho2348")
+    db.session.add(i)
+    db.session.commit()
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    else:
+        return render_template('test.html', info="ok")
+
 
 
 
