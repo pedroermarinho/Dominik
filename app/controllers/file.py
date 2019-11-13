572a626077d427941cae3e4bfc9dc8c4ac0c1abc
Pedro Marinho <pedro.marinho238@gmail.com>
2019-11-12 20:27:28 -0400

    verify_file_yml verify_update_file_yml

--- app/controllers/file.py
+++ app/controllers/file.py
@@ -42,21 +42,17 @@ def download_yml(url, name):
                 sys.stdout.flush()
 
 
-# link = "https://pedroermarinho.github.io/Dominik-dic/src/yml/formally/PT-BR/ai.yml"
-# file_name = directory_default+"/dictionary/yml/Formally/download.data"
-# with open(file_name, "wb") as f:
-#     print("Downloading %s" % file_name)
-#     response = requests.get(link, stream=True)
-#     total_length = response.headers.get('content-length')
-#
-#     if total_length is None:  # no content length header
-#         f.write(response.content)
-#     else:
-#         dl = 0
-#         total_length = int(total_length)
-#         for data in response.iter_content(chunk_size=4096):
-#             dl += len(data)
-#             f.write(data)
-#             done = int(50 * dl / total_length)
-#             sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
-#             sys.stdout.flush()
+def verify_file_yml(yml_data):
+    file = dir_dic_yml + "/" + yml_data["type"][0] + "/" + yml_data["language"][0]
+    file_name = file + "/" + yml_data["subcategory"][0] + ".yml"
+    return os.path.isfile(file_name)
+
+
+def verify_update_file_yml(yml_data):
+    file = dir_dic_yml + "/" + yml_data["type"][0] + "/" + yml_data["language"][0]
+    file_name = file + "/" + yml_data["subcategory"][0] + ".yml"
+    if os.path.isfile(file_name):
+
+        pass
+    else:
+        return None
